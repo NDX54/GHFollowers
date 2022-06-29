21e68ed72e5786db5be9cb0e6fb4a61015a568fa
Jared Juangco <jjuangco01@gmail.com>
2022-06-29 22:20:38 +0800

    Implemented better error handling procedures by using Result<Success, Failure> enum

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -15,14 +15,16 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
-            }
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
-            print("Followers.count = \(followers.count)")
-            print(followers)
+            switch result {
+            case .success(let followers):
+                print(followers)
+            
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
+            }
         }
     }
     
