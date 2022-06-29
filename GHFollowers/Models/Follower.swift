160fa101c6b5ebad97b44108372da4e708fd32ee
Jared Juangco <jjuangco01@gmail.com>
2022-06-29 21:33:35 +0800

    WIP - Network Manager - Long version

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Jared Juangco on 28/6/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Jared Juangco on 28/6/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
